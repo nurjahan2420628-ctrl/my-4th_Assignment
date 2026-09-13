87904314addd7503029e1a6b509a2ae288b86fb3
nurjahan <nurjahan2420628@bscse.uiu.ac.bd>
2026-09-13 18:41:35 +0600

    base add

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/my-4th_Assignment/",
   plugins: [
     react(),
     tailwindcss(),
